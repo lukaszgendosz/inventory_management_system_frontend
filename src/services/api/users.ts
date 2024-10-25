594183e0c477ee2d663ef0cd7d6a5e9ba3a25434
Łukasz Gendosz <lukasz.gendosz@cloudflight.io>
2024-10-25 12:21:28 +0200

    Add user methods

--- src/services/api/users.ts
+++ src/services/api/users.ts
@@ -1,4 +1,4 @@
-import { UsersResponseScheme } from "../../models/user";
+import { UsersResponseScheme, UserCreateScheme, UsersResponseScheme, UserResponseScheme, UserUpdateScheme } from '../../models/user';
 import { ContentType } from "./axios";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { AxiosResponse } from "axios";
@@ -8,6 +8,7 @@ interface queryParams {
   page_size: number | undefined;
   search: string | undefined;
 }
+
 const useUserService = () => {
   const axiosPrivate = useAxiosPrivate();
   
@@ -31,8 +32,67 @@ const useUserService = () => {
       return err;
     } 
   }
+  const getUser = async (userId: number) :Promise<AxiosResponse<UsersResponseScheme, any>> => {
+    try {
+      const response  = await axiosPrivate<UsersResponseScheme>({
+        url: `/api/v1/users/${userId}`,
+        method: "GET",
+        headers: {
+          "Content-Type": ContentType.Json,
+        },
+        params: {
+          page: queryParams.page,
+          page_size: queryParams.page_size,
+          search: queryParams.search,
+        },
+      }); 
+
+      return response;
+    } catch (err: any) {
+      return err;
+    } 
+  }
+  const createUser = async (userAttributes: UserCreateScheme) :Promise<AxiosResponse<UserResponseScheme, any>> => {
+    try {
+      const response  = await axiosPrivate<UserResponseScheme>({
+        url: `/api/v1/users`,
+        method: "POST",
+        headers: {
+          "Content-Type": ContentType.Json,
+        },
+        data: JSON.stringify(userAttributes)
+
+      }); 
+
+      return response;
+    } catch (err: any) {
+      return err;
+    } 
+  }
+
+  const updateUser = async (userId: number, userAttributes: UserUpdateScheme) :Promise<AxiosResponse<UserResponseScheme, any>> => {
+    try {
+      const response  = await axiosPrivate<UserResponseScheme>({
+        url: `/api/v1/users/${userId}`,
+        method: "POST",
+        headers: {
+          "Content-Type": ContentType.Json,
+        },
+        data: JSON.stringify(userAttributes)
+
+      }); 
+
+      return response;
+    } catch (err: any) {
+      return err;
+    } 
+  }
+
   return {
-    getUsers
+    getUsers,
+    getUser,
+    createUser,
+    updateUser
   }
 
 };
